90b797c3a1c635ee92fc52267d188f183325e255
Luis Solís <37953927+solisgb@users.noreply.github.com>
2020-05-31 13:03:38 +0200

    Add files via upload

---
+++ db_connection.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 29 17:48:42 2019
+
+@author: solis
+"""
+
+
+def con_get(dbtype: str, dbpath: str):
+    """
+    conexión a una base de datos
+    """
+    if dbtype == 'ms_access':
+        con = ms_access_conn_get(dbpath)
+        return con
+    elif dbtype == 'sqlite':
+        con = sqlite_conn_get(dbpath)
+        return con
+    elif dbtype == 'postgres':
+        con = postgres_conn_get(dbpath)
+        return con
+    else:
+        raise ValueError(f'El tipo de db {dbtype} no está implementado')
+
+
+def postgres_conn_get(section: str):
+    """
+    devuelve la conexión a una db postgres
+        los parámetros de la conexiona los obtiene del fichero FILE_INI
+        que tiene tantas secciones como bases de datos; cada sección tiene
+        los parámetros: host, database, user, password
+    """
+    import psycopg2
+    from configparser import ConfigParser
+    FILE_INI = 'pgdb.ini'
+    parser = ConfigParser()
+    parser.read(FILE_INI)
+    db_params = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db_params[param[0]] = param[1]
+    else:
+        raise ValueError(f'No se encuentra section {section} en' +\
+                         f' {FILE_INI}')
+    return psycopg2.connect(**db_params)
+
+
+def ms_access_conn_get(dbpath: str):
+    """
+    devuelve la conexión a una db ms access
+    """
+    import pyodbc
+    con_str = r'DRIVER={Microsoft Access Driver ' +\
+    '(*.mdb, *.accdb)}; ' + f'DBQ={dbpath};'
+    return pyodbc.connect(con_str)
+
+
+def sqlite_conn_get(dbpath: str):
+    """
+    devuelve la conexión a una db ms access
+    """
+    import sqlite3
+    return sqlite3.connect(dbpath)
--- splines.py
+++ splines.py
@@ -7,15 +7,23 @@ Created on Sat May 30 20:01:59 2020
 import numpy as np
 
 
-def func01(dir_out: str):
+def func01(dir_out: str, xyg: bool=1):
+    """
+    I select results in table r0 and then I execute
+        update r0 set r0=0.0 where r0<0
+        because some interpolated values are <0
+    xy graphs have the original values but yo can not appreciate values < 0
+        by eye
+    """
     from os.path import join
     import sqlite3
+
     DBNAME = 'r0.db'
     DROP_TABLE = "drop table if exists r0"
     CREATE_TABLE = """
     create table r0 (
     	lat integer,
-        month, integer,
+        month integer,
     	r0 real,
     	primary key (lat, month)
     	)
@@ -33,30 +41,28 @@ def func01(dir_out: str):
     cur.execute(DELETE)
     con.commit()
 
-    for m, xnew, ynew in splines1():
-        for x1, y1 in zip(xnew, ynew):
-            print(f'{x1} {m:n} {y1}')
+    for m, x, y in _splines1():
+        print(m)
+        for x1, y1 in zip(x, y):
             cur.execute(INSERT, (int(x1), m, float(y1)))
-
+        if xyg:
+            _xygraph(dir_out, cur, m)
     con.commit()
     con.close()
 
 
-def splines1():
+def _splines1():
     from scipy import interpolate
 
-    for m, x, y in data1_get():
+    for m, x, y in _data1_get():
         tck = interpolate.splrep(x, y, s=0)
-        xnew = [x[0]]
-        for i in range(1, len(x)):
-            xnew.append((x[i]+x[i-1])/2.)
-        xnew.append(x[-1])
+        xnew = [(x[i]+x[i-1])/2. for i in range(1, len(x))]
         xnew = np.array(xnew, np.float32)
-        ynew = interpolate.splev(xnew, tck, der=0)
-        yield m, xnew, ynew
+        ynew = interpolate.splev(xnew, tck, der=0).astype(np.float32)
+        yield m, np.concatenate((x, xnew)), np.concatenate((y, ynew))
 
 
-def data1_get():
+def _data1_get():
     from db_connection import con_get
 
     con = con_get('postgres', 'bda')
@@ -70,3 +76,29 @@ def data1_get():
         y = np.array([r[1] for r in row], np.float32)
         yield i, x, y
     con.close()
+
+
+def _xygraph(dir_out: str, cur, m: int):
+    """
+    cur: sqlite cursor
+    """
+    import matplotlib.pyplot as plt
+    from os.path import join
+
+    select = f"""
+    select lat, r0
+    from r0
+    where month={m:02n}
+    order by lat
+    """
+
+    cur.execute(select)
+    rs = np.array([r1 for r1 in cur.fetchall()])
+    fig, ax = plt.subplots()
+    ax.plot(rs[:,0], rs[:,1])
+    ax.set(xlabel='lat', ylabel='r0 mm)',
+          title=f'r0 Allen interpolated odd lats month {m:02n}')
+    ax.grid()
+    fig.savefig(join(dir_out,f'r0_month_{m:02n}.png'))
+    plt.close(fig)
+
